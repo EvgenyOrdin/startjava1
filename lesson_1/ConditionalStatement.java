f81614484675645b60d3fcb62337e3a66faafa42
Evgeny Ordin <evge35@gmail.com>
2020-10-13 19:58:23 +0300

    HW Cycle + ConditionalStatement

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -1,35 +1,33 @@
 public class ConditionalStatement {
 	public static void main(String[] args) {
-	int age = 26;
-	boolean manGender = true;
-	float height = 1.70f;
-	char firstNameLetter = 'E';
+		int age = 26;
+		if(age > 20) {
+			System.out.println("мне больше двадцати, мне " + age);
+		}
 
-	if(age > 20) {
-		System.out.println("мне больше двадцати, мне " + age);
-	}
+		boolean manGender = true;
+		if(manGender) {
+			System.out.println("Я мужчина");
+		}
 	
-	if(manGender) {
-		System.out.println("Я мужчина");
-	}
-	
-	if(!manGender) {
-		System.out.println("Я не мужчина");
-	}
-
-	if(height < 1.80) {
-		System.out.println("Я небольшого роста, " + "мой рост " + height);
-	} else {
-		System.out.println("У меня высокий рост, " + "мой рост " + height);
-	 }
-
-	if(firstNameLetter == 'M') {
-		System.out.println("Мое имя начинается с буквы M");
-	} else if(firstNameLetter == 'I') {
-		System.out.println("Мое имя начинается с буквы I");
-	} else {
-		System.out.println("Меня зовут Женя, приятно познакомиться!");
-	}
-
+		if(!manGender) {
+			System.out.println("Я не мужчина");
+		}
+		
+		float height = 1.70f;
+		if(height < 1.80) {
+			System.out.println("Я небольшого роста, " + "мой рост " + height);
+		} else {
+			System.out.println("У меня высокий рост, " + "мой рост " + height);
+	 	}
+		
+		char firstNameLetter = 'E';
+		if(firstNameLetter == 'M') {
+			System.out.println("Мое имя начинается с буквы M");
+		} else if(firstNameLetter == 'I') {
+			System.out.println("Мое имя начинается с буквы I");
+		} else {
+			System.out.println("Меня зовут Женя, приятно познакомиться!");
+		}
 	}
 }
--- lesson_1/Cycle.java
+++ lesson_1/Cycle.java
@@ -1,26 +1,21 @@
 public class Cycle {
 	public static void main(String[] args) {
-		
-		int i = 0;
-		while(i <= 20) {
+		for(int i = 0; i <= 20; i++) {
 			System.out.println(i);
-			i++;
 		}
 
 		int g = 6;
 		while(g >= -6) {
 			System.out.println(g);
-			g-=2;
+			g -= 2;
 		}
 
 		int e = 11;
 		int sum = 0;
 		do {
-			sum = sum + e;
-			e+=2;
+			sum += e;
+			e += 2;
 		} while(e < 20);
 		System.out.println(sum);
-
-
 	}
 }
