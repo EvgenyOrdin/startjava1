2fecce5d4c63bd9e594ec741b0535df25440c326
Evgeny Ordin <evge35@gmail.com>
2020-10-28 23:39:16 +0300

    lesson2 HW_1

---
+++ lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	String name = "John";
+	String gender = "Male";
+	int age = 20;
+	float height = 1.75;
+	float weight = 65.5;
+
+	void move() {
+
+	}
+
+	void run() {
+
+	}
+
+	void sit() {
+
+	}
+
+	String talk() {
+
+	}
+
+	void learnJava() {
+
+	}
+}
