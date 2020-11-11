3626a1511dfd3075319ebb542df09e4d82b904b3
Evgeny Ordin <evge35@gmail.com>
2020-11-11 21:37:30 +0300

    GuessNumber исправления

--- lesson_2/GuessNumber.java
+++ lesson_2/GuessNumber.java
@@ -11,14 +11,8 @@ public class GuessNumber {
 		this.p2 = p2;
 	}
 
-	private void generator() {
-		Random random = new Random();
-		compNum = random.nextInt(100);
-		System.out.println("Компьютер загадал число от 0 до 100!");
-	}
-
 	public void start() {
-		generator();
+		generateNumber();
 		
 		while(true) {
 			Scanner scan = new Scanner(System.in);
@@ -26,33 +20,32 @@ public class GuessNumber {
 			System.out.print(p1.getName() + " введите число: ");
 			p1.setNumber(scan.nextInt());
 
-			boolean check = check(p1.getNumber(), p1.getName());
+			boolean check = compareNumbers(p1.getNumber(), p1.getName());
 
 			if(check) {
 				break;
-			} else {
-				System.out.println(p2.getName() + " ваша очередь");
 			}
 		
 			System.out.print(p2.getName() + " введите число: ");
 			p2.setNumber(scan.nextInt());
 
-			check = check(p2.getNumber(), p2.getName());
+			check = compareNumbers(p2.getNumber(), p2.getName());
 
 			if(check) {
 				break;
-			} else {
-				System.out.println(p1.getName() + " ваша очередь");
-			}
+			}	
 		}
 	}
 
-	private boolean check(int number, String name) {
-		if (compNum > number) {
-				System.out.println(name + "! число компьютера больше чем ваше!");
-				return false;
-			} else if (compNum < number) {
-				System.out.println(name + "! число компьютера меньше чем ваше!");
+	private void generateNumber() {
+		Random random = new Random();
+		compNum = random.nextInt(100);
+		System.out.println("Компьютер загадал число от 0 до 100!");
+	}
+
+	private boolean compareNumbers(int number, String name) {
+		if (compNum != number) {
+				System.out.println(name + (compNum > number ? "! число компьютера больше чем ваше!" : "! число компьютера меньше чем ваше!"));
 				return false;
 			} else {
 				System.out.println(name + ", ВЫ ПОБЕДИЛИ!");
